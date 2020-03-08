cfb80b5cf324871744cc08457aca6d8da22b1c0f
Mikhail Kilin <1kilinmv1@gmail.com>
2020-03-08 16:49:10 +0300

    added styles for bootstrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable, :confirmable,
-    :recoverable, :rememberable, :trackable, :validatable, :lockable
+  devise :database_authenticatable,
+    :confirmable,
+    :recoverable,
+    :rememberable,
+    :trackable,
+    :validatable,
+    :lockable,
+    :registerable
 
   validates :full_name, presence: true
 end
