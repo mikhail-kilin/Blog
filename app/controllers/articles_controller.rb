809f129e3f6026e1baed75520586e90e4b3bc802
Mikhail Kilin <1kilinmv1@gmail.com>
2020-03-10 09:38:41 +0300

    quality changes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :set_article, only: %i[show edit update destroy]
   def new
     @article = Article.new
   end
@@ -11,7 +11,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     if @article.save
-      redirect_to @article, notice: 'Article was successfully created.'
+      redirect_to @article, notice: "Article was successfully created."
     else
       render :new
     end
@@ -19,19 +19,20 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      redirect_to @article, notice: 'Article was successfully updated.'
+      redirect_to @article, notice: "Article was successfully updated."
     else
       render :edit
     end
   end
 
   private
-    def set_article
-      @article = Article.find(params[:id])
-      @article_policy = ArticlePolicy.new
-    end
 
-    def article_params
-      params.require(:article).permit(:title, :content, :status)
-    end
+  def set_article
+    @article = Article.find(params[:id])
+    @article_policy = ArticlePolicy.new
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content, :status)
+  end
 end
