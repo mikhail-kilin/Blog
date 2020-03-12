dae6d8e2406c81c453c66892519a3332f45381cc
Mikhail Kilin <1kilinmv1@gmail.com>
2020-03-12 08:28:33 +0300

    aws amazon integration fixes

--- config/initializers/carier_wave.rb
+++ config/initializers/carier_wave.rb
@@ -1,10 +1,12 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider:              "AWS",
-    aws_access_key_id:     ENV.fetch("AWS_ACCESS_KEY_ID"),
-    aws_secret_access_key: ENV.fetch("AWS_SECRET_ACCESS_KEY_ID"),
-    region:                ENV.fetch("AWS_REGION")
-  }
+  if ENV["AWS_REGION"]
+    config.fog_credentials = {
+      provider:              "AWS",
+      aws_access_key_id:     ENV.fetch("AWS_ACCESS_KEY_ID"),
+      aws_secret_access_key: ENV.fetch("AWS_SECRET_ACCESS_KEY_ID"),
+      region:                ENV.fetch("AWS_REGION")
+    }
+  end
   config.fog_directory  = "rails-blog"
   config.fog_public     = false
   config.fog_attributes = { cache_control: "public, max-age=#{10.days.to_i}" }
