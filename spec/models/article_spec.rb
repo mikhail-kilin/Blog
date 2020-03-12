dd9aebaee6ff5b55c12e017bb66d2cf94d75074d
Mikhail Kilin <1kilinmv1@gmail.com>
2020-03-12 11:59:33 +0300

    small style changes

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Article, type: :model do
 
   it { is_expected.to validate_presence_of :content }
 
-  it "Default value of status is 'draft'" do
+  it "has default 'draft' status" do
     article = Article.create(title: "title", content: "content")
     expect(article.status).to eq("draft")
   end
