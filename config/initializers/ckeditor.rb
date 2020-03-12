6a5a2456142cfe7311bd29f6f9b60534f4e525dd
Mikhail Kilin <1kilinmv1@gmail.com>
2020-03-12 12:30:14 +0300

    changed url to sefety

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -1,4 +1,4 @@
 Ckeditor.setup do |config|
   require "ckeditor/orm/active_record"
-  config.cdn_url = "http://cdn.ckeditor.com/4.3.5/full/ckeditor.js"
+  config.cdn_url = "//cdn.ckeditor.com/4.3.5/full/ckeditor.js"
 end
