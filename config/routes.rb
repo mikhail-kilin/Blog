2ffd8e4810f3ada10de52dc2c63a4a27541b1267
Mikhail Kilin <1kilinmv1@gmail.com>
2020-03-16 15:08:50 +0300

    removed sign_up and cancel account from user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   mount Ckeditor::Engine => "/ckeditor"
   resources :articles, except: :destroy
-  devise_for :users, controllers: { registrations: "users/registrations" }
+
+  devise_for :users, skip: [:registrations]
+  devise_scope :user do
+    resource :users,
+      only: %i[edit update],
+      controller: "users/registrations",
+      as: :user_registration
+  end
+
   root to: "pages#home"
 end
