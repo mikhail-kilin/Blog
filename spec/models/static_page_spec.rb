d78f3161343f86acf0d6d22737f108fd588a0f92
Mikhail Kilin <1kilinmv1@gmail.com>
2020-03-23 22:09:23 +0300

    review changes

--- spec/models/static_page_spec.rb
+++ spec/models/static_page_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe StaticPage, type: :model do
+RSpec.describe StaticPage do
   it { is_expected.to validate_presence_of :title }
 
   it { is_expected.to validate_presence_of :content }
