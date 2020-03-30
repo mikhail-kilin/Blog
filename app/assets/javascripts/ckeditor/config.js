4fa6e197bae15ace791108b4665f957662bf3d88
Mikhail Kilin <1kilinmv1@gmail.com>
2020-03-30 14:35:36 +0300

    chnged config.js

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,23 +1,25 @@
-CKEDITOR.editorConfig = function(config) {
-    config.filebrowserBrowseUrl = "/ckeditor/attachment_files";
-    config.filebrowserFlashBrowseUrl = "/ckeditor/attachment_files";
-    config.filebrowserFlashUploadUrl = "/ckeditor/attachment_files";
-    config.filebrowserImageBrowseLinkUrl = "/ckeditor/pictures";
-    config.filebrowserImageBrowseUrl = "/ckeditor/pictures";
-    config.filebrowserImageUploadUrl = "/ckeditor/pictures";
-    config.filebrowserUploadUrl = "/ckeditor/attachment_files";
-    config.toolbarGroups = [
-		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-		{ name: 'links' },
-		{ name: 'insert' },
-		{ name: 'tools' },
-		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'others' },
-		'/',
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks' ] },
-		{ name: 'about' }
-    ];
-    return true;
-};
+if (document.getElementsByClassName("ckeditor").length > 0) {
+	CKEDITOR.editorConfig = function(config) {
+		config.filebrowserBrowseUrl = "/ckeditor/attachment_files";
+		config.filebrowserFlashBrowseUrl = "/ckeditor/attachment_files";
+		config.filebrowserFlashUploadUrl = "/ckeditor/attachment_files";
+		config.filebrowserImageBrowseLinkUrl = "/ckeditor/pictures";
+		config.filebrowserImageBrowseUrl = "/ckeditor/pictures";
+		config.filebrowserImageUploadUrl = "/ckeditor/pictures";
+		config.filebrowserUploadUrl = "/ckeditor/attachment_files";
+		config.toolbarGroups = [
+			{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+			{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+			{ name: 'links' },
+			{ name: 'insert' },
+			{ name: 'tools' },
+			{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+			{ name: 'others' },
+			'/',
+			{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+			{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks' ] },
+			{ name: 'about' }
+		];
+		return true;
+	};
+}
