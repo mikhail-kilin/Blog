ed1babfd34f0aec534568580cc5eedc2b0159f6e
Mikhail Kilin <1kilinmv1@gmail.com>
2020-04-10 13:03:12 +0300

    added slug generator

--- app/assets/javascripts/slug/slug_generator.js
+++ app/assets/javascripts/slug/slug_generator.js
@@ -2,6 +2,10 @@ function static_page_replace() {
     $('#static_page_slug').val(generate($('#static_page_title').val()));
 }
 
+function reg_replace() {
+    $('#user_company_slug').val(generate($('#user_company_name').val()));
+}
+
 function generate(text) {
     return text.split(' ').join('-').toLowerCase()
 }
