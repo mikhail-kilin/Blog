c7d3e9d72fd75efaf62be6574ce82dd37b24f2fd
Mikhail Kilin <1kilinmv1@gmail.com>
2020-04-15 17:55:14 +0300

    chanhed routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,9 @@ Rails.application.routes.draw do
   resources :articles, only: :show do
     resources :comments, except: %i[index new]
   end
-  # resources :static_pages, only: :show
 
   namespace :admin_scope do
     resources :articles
-    # resources :static_pages
     resources :companies, except: %i[index destroy]
     resource :article_status, only: :update
   end
