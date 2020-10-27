7f82d5fd629731c21f61f57e7007cdf2cc84f66e
Mikhail Kilin <1kilinmv1@gmail.com>
2020-10-27 22:20:29 +0300

    fix tests

--- spec/features/visitor/article/index_spec.rb
+++ spec/features/visitor/article/index_spec.rb
@@ -1,57 +1,49 @@
 require "rails_helper"
 
 feature "View Articles" do
-  let!(:user) { FactoryBot.create :user }
-  let!(:company) { FactoryBot.create :company, owner: user }
-  let(:draft) { FactoryBot.create :article, :draft, :company, user: user, company: company }
+  let(:user) { create :user }
+  let(:company) { create :company, owner: user }
+  let!(:draft_article) { create :article, :draft, :company, user: user, company: company }
+  let!(:published_article) { create :article, :published, :company, user: user, company: company }
 
-  background do
-    FactoryBot.create_list :article, 6, :published, :company, user: user, company: company
-    FactoryBot.create :user
+  before do
     visit company_path(company)
   end
 
-  scenario "Visitor can see sorted paginated articles" do
-    first_title = Article.order(created_at: :desc).first.title
-    last_title = Article.order(created_at: :desc).last.title
-
-    expect(page).to have_content(first_title)
-    expect(page).not_to have_content(last_title)
-
-    click_on "Last »"
-
-    expect(page).not_to have_content(first_title)
-    expect(page).to have_content(last_title)
-  end
-
   scenario "Visitor can see only published articles" do
-    expect(page).not_to have_content(draft.title)
+    expect(page).not_to have_content(draft_article.title)
+    expect(page).to have_content(published_article.title)
   end
 
-  scenario "Visitor can see search articles by title" do
-    article = Article.order(created_at: :desc).last
+  context "we have 20 articles" do
+    let!(:list_articles) { create_list :article, 20, :published, user: user, company: company }
 
-    expect(page).not_to have_content(article.title)
+    scenario "Visitor can see search articles by title" do
+      article = list_articles.last
 
-    first_title_word = article.title.split(" ").last
+      expect(page).not_to have_content(article.title)
 
-    fill_in :search_data, with: first_title_word
+      first_title_word = article.title.split(" ").last
 
-    click_on "Search"
+      fill_in :search_data, with: first_title_word
 
-    expect(page).to have_content(article.title)
-  end
+      click_on "Search"
 
-  scenario "Visitor can see search articles by title" do
-    article = Article.order(created_at: :desc).last
-    expect(page).not_to have_content(article.title)
+      expect(page).to have_content(article.title)
+    end
 
-    first_content_word = article.content.split(" ").last
+    scenario "Visitor can see search articles by content" do
+      article = list_articles.last
 
-    fill_in :search_data, with: first_content_word
+      expect(page).not_to have_content(article.title)
 
-    click_on "Search"
+      first_content_word = article.content.split(" ").last
 
-    expect(page).to have_content(article.title)
+      fill_in :search_data, with: first_content_word
+
+      click_on "Search"
+
+      expect(page).to have_content(article.title)
+    end
   end
 end
